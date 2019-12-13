e8cd4e1f81b9db0ecf3fa5a557d8a89a2e9b9d94
vhe9018 <donat.udvari@urss.hu>
2019-12-13 10:36:29 +0100

    websocket fix

--- don-chess/src/app/shared/service/web-socket.service.ts
+++ don-chess/src/app/shared/service/web-socket.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 const SockJs = require('sockjs-client');
 const Stomp = require('stompjs/lib/stomp.js').Stomp;
 
 @Injectable()
 export class WebSocketService {
+    route = environment.apiUrl === '' ? 'http://localhost:8080' : environment.apiUrl; 
 
     // Open connection with the back-end socket
     public connect() {
-        const socket = new SockJs(`http://localhost:8080/socket`);
+        const socket = new SockJs(this.route + '/socket');
 
         const stompClient = Stomp.over(socket);
 
